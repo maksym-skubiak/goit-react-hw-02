97cef70b2b18f8b3b6e0b06e4bc773ac85e5585f
Maksym Skubiak <skubiakcreates@gmail.com>
2025-03-28 12:00:54 +0100

    commit

--- src/Feedback/Feedback.jsx
+++ src/Feedback/Feedback.jsx
@@ -1,13 +1,12 @@
-import css from "./Feedback.module.css";
 function Feedback({ feedback, totalFeedback, positiveFeedback }) {
   return (
     <>
-      <ul className={css.list}>
-        <li className={css.item}>Good: {feedback.good}</li>
-        <li className={css.item}>Neutral: {feedback.neutral}</li>
-        <li className={css.item}>Bad: {feedback.bad}</li>
-        <li className={css.item}>Total: {totalFeedback}</li>
-        <li className={css.item}>Positive: {positiveFeedback}</li>
+      <ul>
+        <li>Good: {feedback.good}</li>
+        <li>Neutral: {feedback.neutral}</li>
+        <li>Bad: {feedback.bad}</li>
+        <li>Total: {totalFeedback}</li>
+        <li>Positive: {positiveFeedback}</li>
       </ul>
     </>
   );
